929e0077cce1fc3129b9b0ffcbb8ab82482302bc
Saumya Sindhu <saumyasindhu75@gmail.com>
2026-08-01 18:15:23 +0530

    Stage 5: Swagger UI

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import taskRouter from "./routes/tasks.routes.js";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./docs/openapi.json" with { type: "json" };
+
 const app = express();
 
 app.use(express.json());
 app.use("/tasks", taskRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
     res.json({
