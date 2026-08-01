acdd5f4785b2de889206b78de279063f49fbdee4
Saumya Sindhu <saumyasindhu75@gmail.com>
2026-08-01 17:03:43 +0530

    started server

---
+++ Backend/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.send({
+        status: "Ok"
+    });
+})
+
+export default app;
